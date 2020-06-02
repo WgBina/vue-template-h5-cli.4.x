ced1a8a1dfe1e14e57721c16883073721158a07e
bing.wan <1078827038@qq.com>
2020-06-02 11:46:58 +0800

    xiugai

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="index">
-    <van-nav-bar
-      title="标题"
-      left-text="返回"
-      right-text="按钮"
-      left-arrow
-      @click-left="onClickLeft"
-      @click-right="onClickRight"
-    />
 
     <div class="section">
       <div class="content">
