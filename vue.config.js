e5c011d435faa2293bd8656b9c6bcbb5c45ae14f
bing.wan <1078827038@qq.com>
2020-10-14 13:39:43 +0800

    添加配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
+const Timestamp = new Date().getTime();
+
 module.exports = {
     publicPath: './',
     outputDir: 'dist',
+
+    configureWebpack: {
+        output: {
+            // 输出重构【模块名称.hash值.时间戳】
+            filename: `[name].[hash].${Timestamp}.js`,
+            chunkFilename: `[name].[hash].${Timestamp}.js`
+        }
+    },
 
     lintOnSave: false,  //关闭eslint校验
 
